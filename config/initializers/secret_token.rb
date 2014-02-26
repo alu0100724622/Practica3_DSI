c859e087d3379ea288ae93bdb03b53d107d8ebeb
Noelia Rodriguez Martin <alu0100724622@ull.edu.es>
2014-02-26 10:36:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practica3Dsi::Application.config.secret_key_base = '94b6305ddd6b05001427b8511601e4bbdf333706dacbb202774b29e468ad9e67726654ad22d0cd4bc9c355a8f64ca0f8e64687fdaa100cb787a4b8e241fd750c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Practica3Dsi::Application.config.session_store :cookie_store, key: '_practica3_DSI_session'
